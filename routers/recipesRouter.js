748300dad4386551930cdc36e7c2110c916e70ce
unknown <neduvinci@gmail.com>
2019-11-07 17:21:32 +0100

    Tested recipes endpoint

--- routers/recipesRouter.js
+++ routers/recipesRouter.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const recipes = require('../data/helpers')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json("Hello from the recipes man thing")
+    recipes.getRecipes()
+        .then(recipes => {
+            if (recipes.length) {
+                res.status(200).json(recipes)
+            }
+            else {
+                res
+                    .status(404)
+                    .json({
+                        message: "There are no saved recipes"
+                    })
+            }
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                    message: "There was an error getting the recipes list " + " " + error
+                })
+        })
 })
 
 module.exports = router
