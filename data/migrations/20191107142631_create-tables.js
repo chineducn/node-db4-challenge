6e55aa1b5b844c507a8c9ab49ee886e2757adc13
unknown <neduvinci@gmail.com>
2019-11-07 18:45:58 +0100

    made code in migration file a bit cleaner

--- data/migrations/20191107142631_create-tables.js
+++ data/migrations/20191107142631_create-tables.js
@@ -1,33 +1,44 @@
 
 exports.up = function(knex) {
-    return knex.schema
+    return knex
+        .schema
         .createTable('recipes', tbl => {
-            tbl.increments();
-            tbl.string('name', 255)
+            tbl
+                .increments();
+            tbl
+                .string('name', 255)
                 .unique()
                 .notNullable();
         })
         .createTable('ingredients', tbl => {
-            tbl.increments();
-            tbl.string('name', 255)
+            tbl
+                .increments();
+            tbl
+                .string('name', 255)
                 .unique()
                 .notNullable();
-            tbl.float('quantity', 5, 2)
+            tbl
+                .float('quantity', 5, 2)
                 .notNullable();
         })
         .createTable('steps', tbl => {
-            tbl.increments();
-            tbl.integer('step_number')
+            tbl
+                .increments();
+            tbl
+                .integer('step_number')
                 .unsigned()
                 .notNullable()
-            tbl.string('description', 255)
+            tbl
+                .string('description', 255)
                 .notNullable()
-            tbl.integer('ingredient_id')
+            tbl
+                .integer('ingredient_id')
                 .unsigned()
                 .notNullable()
                 .references('id')
                 .inTable('ingredients')
-            tbl.integer('recipe_id')
+            tbl
+                .integer('recipe_id')
                 .unsigned()
                 .notNullable()
                 .references('id')
@@ -36,7 +47,8 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema
+    return knex
+        .schema
         .dropTableIfExists('steps')
         .dropTableIfExists('ingredients')
         .dropTableIfExists('recipes')
