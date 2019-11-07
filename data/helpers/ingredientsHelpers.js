56bd7032ce959d99ae9171420fc14e19c38055f8
unknown <neduvinci@gmail.com>
2019-11-07 20:11:04 +0100

    Made some clean up in ingredientsHelpers. Stretch complete

--- data/helpers/ingredientsHelpers.js
+++ data/helpers/ingredientsHelpers.js
@@ -6,8 +6,19 @@ module.exports = {
 
 function getRecipesContainingIngredient(ingredient_id) {
    return db('steps as s')
-        .join('recipes as r', 'r.id', 's.recipe_id')
-        .join('ingredients as i', 'i.id', 's.ingredient_id')
-        .select('i.name as Ingredient', 'r.name as Recipe')
+       .join(
+           'recipes as r',
+           'r.id',
+           's.recipe_id'
+       )
+       .join(
+           'ingredients as i',
+           'i.id',
+           's.ingredient_id'
+       )
+       .select(
+           'i.name as Ingredient',
+           'r.name as Recipe'
+       )
         .where({ ingredient_id: ingredient_id })
 }
